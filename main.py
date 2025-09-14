435f12cc3162eb6c53799ac8ae168d2ddb249a04
Anuradha Menon <anuradhameno@gmail.com>
2025-09-14 20:20:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import streamlit as st
 from utils.db_connection import init_db
 
 init_db()
-st.title("Cricbuzz LiveStats")
+st.title("Cricbuzz LiveStats - Created by Anuradha Menon")
 st.write("Welcome to the cricket analytics dashboard!")
 st.write("Use the sidebar to navigate to Live Matches, Top Stats, SQL Analytics, or CRUD Operations.")
